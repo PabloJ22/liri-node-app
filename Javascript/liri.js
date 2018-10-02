6152119df256796eda50ca74cdceec88b0203827
APabloJ <altisimop@gmail.com>
2018-10-01 22:17:46 -0500

    added a movie function

--- Javascript/liri.js
+++ Javascript/liri.js
@@ -61,30 +61,69 @@ request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tril
     }
 });
 
+
+
+// This function will have the user enter a movie title and pull information which will print to the terminal.
+function movieFunction() {
+    //console.log when entering the function
+    console.log("Entering Movie Function");
+
+    //variable to store the movie title the user inputs
+    //if user does not put in any input the movie will default to Mr.Nobody
+    var movieTitle;
+    if (alternateUserInput === undefined) {
+        movieTitle = "Mr. Nobody";
+    } else {
+        movieTitle = alternateUserInput;
+    };
+
+
+    //Request Code line found in OMDB in class assignment
+    //var movieTitle is put in the middle of the URL where the movie title is requested
+    request("http://www.omdbapi.com/?t=" + movieTitle + "&plot=short&r=json &tomatoes=true", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            //an alterative to using JSON.parse(body) for all console.logs would be to create a variable =JSON.parse(body).  e.x. console.log("The movie's title is: " + variable.Title);
+            //Model Fields found at = https://media.readthedocs.org/pdf/omdbpy/latest/omdbpy.pdf
+            console.log("The movie's title is: " + JSON.parse(body).Title);
+            console.log("The movie was released in: " + JSON.parse(body).Year);
+            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+            console.log("This movie was produced in: " + JSON.parse(body).Country);
+            console.log("This movie is available in: " + JSON.parse(body).Language);
+            console.log("This movie's plot is:  " + JSON.parse(body).Plot);
+            console.log("The movie has the following actors: " + JSON.parse(body).Actors);
+            //ROTTEN TOMATOES RATING AND WEBSITE
+            console.log("The Rotten Tomatoes score for this film is " + JSON.parse(body).tomatoRating);
+            console.log("The Rotten Tomatoes URL for this film is " + JSON.parse(body).tomatoURL);
+        }
+    });
+    //The user input will append to the log.txt file(ex. movie-this + movie title)
+    fs.appendFile("log.txt", ", " + userInput + " " + alternateUserInput);
+};
+
 //spotify api
 
 //search: 
-function ({
-    type: 'artist OR album OR track',
-    query: 'My search query',
-    limit: 20
-}, callback);
+// function ({
+//     type: 'artist OR album OR track',
+//     query: 'My search query',
+//     limit: 20
+// }, callback);
 
 
-var Spotify = require('node-spotify-api');
+// var Spotify = require('node-spotify-api');
 
-var spotify = new Spotify({
-    id: < your spotify client id > ,
-    secret: < your spotify client secret >
-});
+// var spotify = new Spotify({
+//     id: < your spotify client id > ,
+//     secret: < your spotify client secret >
+// });
 
-spotify.search({
-    type: 'track',
-    query: 'All the Small Things'
-}, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+// spotify.search({
+//     type: 'track',
+//     query: 'All the Small Things'
+// }, function (err, data) {
+//     if (err) {
+//         return console.log('Error occurred: ' + err);
+//     }
 
-    console.log(data);
-});
+//     console.log(data);
+// });
